88ca5501c227d4145f9354e1d6994c9fa24d102e
StrangeEcho <tylergoodmen312@gmail.com>
2025-02-19 15:15:44 -0500

    Revert "Delete Unnecessary Files, Add Simple Component"
    This reverts commit a38678684f60c3c46614989e5857dc2df67425d0.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { useState } from 'react';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-  const [index, setIndex] = useState(0);
-
-
-  function handleIncrement() {
-    setIndex(index + 1);
-  }
-
   return (
-    <div>
-      <h1>Clicks: {index}</h1>
-      <button onClick={handleIncrement}>Click Me To Increment</button>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
 export default App;
